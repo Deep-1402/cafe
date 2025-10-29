9f0204120e8914d8779223619dd884fd3174acc3
dev-patel02 <deepkumar.patel@netclues.in>
2025-10-29 20:08:31 +0530

    Create Apis for RBAC

--- backend/routes/tenant.js
+++ backend/routes/tenant.js
@@ -2,17 +2,22 @@ import express from "express";
 import tenant from "../controllers/tenant/tenant.js";
 import authorize from "../middleware/authentication.js";
 import Admin from "../middleware/admin.js";
-import roles from "../controllers/tenant/roles.js";
+
 
 const router = express.Router();
 
 router.post("/login", tenant.login);
-router.post("/user-create", [authorize, Admin], tenant.createUser);
+router.post("/user", [authorize, Admin], tenant.createUser);
 //role
-router.post("/role-create", [authorize, Admin], tenant.createRole);
+router.post("/role", [authorize, Admin], tenant.createRole);
 //module
-router.post("/module-create", [authorize, Admin], tenant.createModule);
+router.post("/module", [authorize, Admin], tenant.createModule);
 //permission
-router.post("/permission-create", [authorize, Admin], tenant.createPermission);
+router.post("/permission", [authorize, Admin], tenant.createPermission);
+
+// Catgory
+router.post("/catagory", authorize,  )
+
+// Menu
 
 export default router;
