2b892c481adcdbe4a1570f8bf702c3375ee60802
Patel Deep <121955130+Deep-1402@users.noreply.github.com>
2025-11-05 22:16:46 +0530

    Delete backup directory

--- backup/_backend/routes/tenant.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import tenant from "../controllers/tenant.js";
-
-const router = express.Router();
-
-// router.post("/login", tenant.login);
-
-export default router;
